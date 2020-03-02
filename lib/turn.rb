ea6111bcc8f2e8c8c24f9ca5be9c7c0550001555
Josh Ellis <joshe523@gmail.com>
2020-03-02 04:17:48 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,28 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "---------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "---------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  if index < 0 || index > 8
+    return false
+  elsif position_taken?(board, index)
+    return false
+  end
+  return true
+end
+
+def position_taken?(board, index)
+  v = board[index]
+  if v == ' ' || v == '' || v == nil
+    return false
+  end
+  return true
+end
+
+def move(board, index, token = "X")
+  board[index] = token
+end
