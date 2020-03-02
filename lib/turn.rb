ae454a335608fbcb807f45a90a858090fc4fd224
Josh Ellis <joshe523@gmail.com>
2020-03-02 04:40:16 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -37,7 +37,9 @@ def turn(board)
   index = input_to_index(user_input)
   loop do
     if valid_move?(board,index)
+      puts "that works"
       move(board,index)
+      display_board(board)
       return
     else
       puts "Invalid input. Please try again:"
